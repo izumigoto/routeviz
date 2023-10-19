80bf2ce7a4261d44a2adbd436bb69c82641c0da4
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-10-19 23:42:06 +0530

    :sparkles: Added more pathfinding algo's

--- src/pathfinder/main.py
+++ src/pathfinder/main.py
@@ -1,10 +1,6 @@
 import time
 from typing import Callable
 
-from .search.astar import AStarSearch
-from .search.gbfs import GreedyBestFirstSearch
-from .search.bfs import BreadthFirstSearch
-from .search.dfs import DepthFirstSearch
 from .search.dijkstras import DijkstrasSearch
 from .models.grid import Grid
 from .models.solution import Solution
@@ -14,11 +10,7 @@ from .models.search_types import Search
 SearchFunction = Callable[[Grid], Solution]
 
 SEARCH: dict[Search, SearchFunction] = {
-    Search.ASTAR_SEARCH: AStarSearch.search,
-    Search.DIJKSTRAS_SEARCH: DijkstrasSearch.search,
-    Search.BREADTH_FIRST_SEARCH: BreadthFirstSearch.search,
-    Search.GREEDY_BEST_FIRST_SEARCH: GreedyBestFirstSearch.search,
-    Search.DEPTH_FIRST_SEARCH: DepthFirstSearch.search,
+    Search.DIJKSTRAS_SEARCH: DijkstrasSearch.search
 }
 
 
